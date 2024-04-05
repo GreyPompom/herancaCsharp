2e22077ac59ceb1e4ee306f44407bade728af476
Emely - Desenvolvimento 06 <desenvolvimento06@pauta.com.br>
2024-04-05 10:21:48 -0300

    full project working

---
+++ Entities/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace herancaCsharp.Entities
+{
+    class Employee
+    {
+        public String Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee() { }
+        public Employee(String name, int hours, double valuePH)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePH;
+        }
+
+        public virtual double Payment()
+        {
+            return ValuePerHour * Hours;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder cs = new StringBuilder();
+            cs.AppendLine(Name + " - $ "+ Payment().ToString("F2", CultureInfo.InvariantCulture));
+            return cs.ToString();
+        }
+    }
+}
